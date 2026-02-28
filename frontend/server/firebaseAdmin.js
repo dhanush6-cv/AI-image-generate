bdef6205224b44cecf6b2e07fd634d0a92615fd2
dhanush <sekhardhanush6@gmail.com>
2026-02-28 23:12:53 +0530

    use FIREBASE_SERVICE_ACCOUNT env instead of json file

--- frontend/server/firebaseAdmin.js
+++ frontend/server/firebaseAdmin.js
@@ -1,10 +1,14 @@
-// server/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+function getServiceAccount() {
+  const raw = process.env.FIREBASE_SERVICE_ACCOUNT;
+  if (!raw) throw new Error("FIREBASE_SERVICE_ACCOUNT is missing in env");
+  return JSON.parse(raw);
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(getServiceAccount()),
   });
 }
 
