f417144a80d2479abce53725cdde85c5f28aba6d
dhanush <sekhardhanush6@gmail.com>
2026-03-01 21:11:31 +0530

    fix firebase admin export

--- frontend/server/firebaseAdmin.js
+++ frontend/server/firebaseAdmin.js
@@ -1,17 +1,13 @@
 const admin = require("firebase-admin");
 
-/*
-  Render environment variable la
-  FIREBASE_SERVICE_ACCOUNT nu full JSON string store panniruppa
-*/
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Firebase initialize (once only)
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
-module.exports = admin;
+const db = admin.firestore();
+
+module.exports = { admin, db };
